0623662ac167f179deb65c94196f86a9cbbcb6c8
Henrique Camargo nobrega <Henriquecnobrega0711@gmail.com>
2024-12-05 12:51:16 -0300

    notebook changes

--- app/src/main/java/com/example/kotlindozero/VariaveisConstantes.kt
+++ app/src/main/java/com/example/kotlindozero/VariaveisConstantes.kt
@@ -1,4 +1,13 @@
 package com.example.kotlindozero
+fun correr(){
+
+}
+
+const val nome = "Henrique"
+
+class Usuario {
+
+}
 
 fun main() {
 
@@ -6,4 +15,10 @@ fun main() {
         val n2 = 30
         val n3 = n1 + n2
         println("O resultado do n1 + n2 e: " + n3)
+        println(nome)
+
+        val numero: Int = 20
+        val usuario = Usuario()
+
+
 }
