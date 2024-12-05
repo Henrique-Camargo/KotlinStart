d339b9b5cc234d2e614599fe2d7dd094508babb8
Henrique Camargo nobrega <Henriquecnobrega0711@gmail.com>
2024-12-05 12:35:22 -0300

    Estudando Kotlin

--- app/src/main/java/com/example/kotlindozero/VariaveisConstantes.kt
+++ app/src/main/java/com/example/kotlindozero/VariaveisConstantes.kt
@@ -5,5 +5,5 @@ fun main() {
         val n1 = 20
         val n2 = 30
         val n3 = n1 + n2
-        println("O resultado do N1 + N2 e: " + n3)
+        println("O resultado do n1 + n2 e: " + n3)
 }
