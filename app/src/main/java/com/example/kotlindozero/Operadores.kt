5e078809c69d11ff1e88c1c88ff4467978cbed51
Henrique Camargo nobrega <Henriquecnobrega0711@gmail.com>
2024-12-06 17:00:39 -0300

    notebook changes

--- app/src/main/java/com/example/kotlindozero/Operadores.kt
+++ app/src/main/java/com/example/kotlindozero/Operadores.kt
@@ -9,9 +9,32 @@ fun main() {
     val resultadoMult = n1 * n2 //multiplicação
     val resultadoSob = n1 % n2 //sobra da divisão
 
+    val resultadoIgual = n1 == n2 //Igual
+    val resultadoDiferente = n1 != n2 // diferente
+    val resultadoMaior = n1 > n2 // maior
+    val resultadoMenor = n1 < n2 // menor
+    val resultadoMaiorIgual = n1 >= n2 //maior ou igual
+    val resultadoMenorIgual = n1 <= n2 // menor ou igual
+
+    val resultadoOu = true || true // seria equivalente ao OU, um ou outro ser verdadeiro
+    val resultadoE = true && true // seria equivalente a os 2 serem iguais
+    val resultadoNegativo = true && true // negação na visualição do resultado tem um ! ele inverte a respota
+
     println(resultado)
     println(resultadoSub)
     println(resultadoDiv)
     println(resultadoMult)
     println(resultadoSob)
+
+    println(resultadoIgual)
+    println(resultadoDiferente)
+    println(resultadoMaior)
+    println(resultadoMenor)
+    println(resultadoMaiorIgual)
+    println(resultadoMenorIgual)
+
+    println(resultadoOu)
+    println(resultadoE)
+    println(!resultadoNegativo) //apenas adicionar o ! ele inverte a resposta tanto do && tanto do ||
+
 }
---
+++ app/src/main/java/com/example/kotlindozero/TesteCompras.kt
@@ -0,0 +1,11 @@
+package com.example.kotlindozero
+
+fun main() {
+
+    val idade = 18
+    val compras = 300
+
+    val desconto = idade >= 20 || compras >= 250
+
+    println(desconto)
+}
